f06e06533210841ec7d849b0f21745f18a130fb0
Elin Axelsson <elinaxel@gmail.com>
2015-11-20 15:28:21 +0100

    Added blast app

---
+++ app4/functions.R
@@ -0,0 +1,32 @@
+t2g = read.table("../data/blast/trans2gene.tab")
+rownames(t2g)=t2g$V1
+colnames(t2g)=c("transcript","gene","scaffold","start","end")
+
+cmdCreate <- function(infile, outfile,type){
+  if(type=="1"){
+    return(paste("tblastx -db ../data/blast/merged.fa -query ", infile, " -outfmt 7",  " > ",
+                 outfile, sep = ""))
+  }
+  if(type=="2")
+    return(paste("tblastn -db ../data/blast/merged.fa -query ", infile, "-num_alignments 5 -outfmt 7",  " > ",
+                 outfile, sep = ""))
+}
+
+createLink <- function(val) {
+  sprintf('<a href=%s target="_blank" class="btn btn-primary">View</a>',val)
+}
+
+createFA <- function(id){
+  outfile=paste("fasta",id,".fa",sep="")
+  paste("pyfasta extract --header --fasta path2fasta", id, " > ",
+        outfile, sep = "")
+}
+map2gene <- function(id){
+  g = t2g[id,"gene"]
+  g
+}
+
+mapTranscriptSeq <- function(id,outfile){
+  g = t2g[id,c("scaffold","start","end")]
+  paste("/Library/samtools-0.1.19/samtools faidx ../data/blast/Marchantia_polymorpha.main_genome.scaffolds.fasta",paste(g[,1],paste(g[,2],g[,3],sep="-"),sep=":"))#,">",outfile) 
+}
---
+++ app4/server.R
@@ -0,0 +1,66 @@
+library(shiny)
+library(DT)
+source("functions.R")
+hits=NULL
+shinyServer(function(input, output) {
+  
+    output$Results <- DT::renderDataTable({
+      input$action
+      progress <- shiny::Progress$new()
+      progress$set(message = "Computing data", value = 0)
+      # Close the progress when this reactive exits (even if there's an error)
+      on.exit(progress$close())
+      
+      # Create a callback function to update progress.
+      # Each time this is called:
+      # - If `value` is NULL, it will move the progress bar 1/5 of the remaining
+      #   distance. If non-NULL, it will set the progress to that value.
+      # - It also accepts optional detail text.
+      updateProgress <- function(value = NULL, detail = NULL) {
+        if (is.null(value)) {
+          value <- progress$getValue()
+          value <- value + (progress$getMax() - value) / 5
+        }
+        progress$set(value = value, detail = detail)
+      }
+        data=(input$text)
+        #print(data)
+        #if (is.null(data)){
+         # return(NULL)
+        #}
+        if (data!=""){
+            print("ok")
+            con=file("temp.fa", open = "w")
+            writeLines(c(">Q1",data),con=con)
+            close(con)
+            print("oo")
+            print(input$radio)
+            cmds <- cmdCreate("temp.fa","out.out",input$radio)
+            print(cmds)
+            system(cmds)
+            my_table=as.data.frame(system2("bash", "../script/blast/grepCmd.sh",stdout=TRUE))
+            print(my_table)
+            my_table$link <- createLink(my_table[,1])
+            ex = read.table("out.out")
+            gene = map2gene(ex[,2])
+            my_table=cbind(gene,ex[,-1])#,my_table$link)
+            hits<<- my_table[,2]
+            colnames(my_table)=c("gene_id","transcript_id","% identity","alignment length", "mismatches", "gap opens","q. start" ,"q. end","s. start" ,"s. end","evalue","bit score") #,"igv link")
+        } else
+            return(NULL)
+        my_table
+        },options = list(lengthMenu = c(15, 30, 50), pageLength = 15),escape=FALSE,
+    callback = JS
+        ('table.on("click.dt", "tr", function() {
+        tabs = $(".tabbable .nav.nav-tabs li a");
+        $(tabs[1]).click();})'))
+    
+    output$seq <- renderPrint({
+      sel = hits[as.numeric(input$Results_rows_selected[length(input$Results_rows_selected)])]
+      seq = mapTranscriptSeq(sel,'seq.fasta')
+      ff = system(seq,intern = TRUE)
+      ff
+      
+    })
+    
+})
